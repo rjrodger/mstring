f96e3a6e4c52f553bc2b42e3107609b827ad46e6
david kaye <dfkaye@yahoo.com>
2013-10-01 11:02:57 -0700

    allow for whitespace-indented lines

--- mstring.js
+++ mstring.js
@@ -1,27 +1,32 @@
 /* Copyright (c) 2012 Richard Rodger */
 
-var errmsg = "mstring: required format is function (){/*** ... ***/}, this is invalid: "
+var errmsg = "mstring: required format is function (){/*** ... ***/}, this is invalid: ";
 
 module.exports = function(f){
   if( !_.isFunction(f) ) {
     throw new Error(errmsg+f)
   }
 
-  var fs = f.toString()
-  var m  = fs.match(/^function\s*\(\)\s*\{\s*\/\*\*\*\n*([\s\S]*)\n*\*\*\*\/\s*\}$/);
-  
+  // allow for whitespace-indented lines
+  var lines = f.toString().split('\n');
+  for (var i = 0; i < lines.length; ++i) {
+    lines[i] = lines[i].replace(/^\s+|\s+$/g, '');
+  }
+
+  var fs = lines.join('\n');
+  var m  = fs.match(/^function\s*\(\)\s*\{\s*\/\*\*\*\n([\s\S]*)\n\*\*\*\/\s*\}$/) 
+
   if( m && _.isString(m[1]) ) {
     return m[1]
   }
-  else throw new Error(errmsg+f);
+  else throw new Error(errmsg+f)
 }
 
 
-var _ = {}
+var _ = {};
 _.isFunction = function(obj) {
-  return !!(obj && obj.constructor && obj.call && obj.apply);
-};
+  return !!(obj && obj.constructor && obj.call && obj.apply)
+}
 _.isString = function(obj) {
-  return !!(obj === '' || (obj && obj.charCodeAt && obj.substr));
-};
-
+  return !!(obj === '' || (obj && obj.charCodeAt && obj.substr))
+}
